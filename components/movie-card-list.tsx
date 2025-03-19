91b411ebc1451e5f9b332f3fe023e673884d180d
thayoon <thagyun@naver.com>
2025-03-20 01:20:20 +0900

    feat: infiniteQuery, generateMetadata

--- components/movie-card-list.tsx
+++ components/movie-card-list.tsx
@@ -1,25 +1,48 @@
 "use client";
 
 import MovieCard from "./movie-card";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { searchMovies } from "actions/movieActions";
 import { Spinner } from "@material-tailwind/react";
 import { searchState } from "utils/recoil/atoms";
 import { useRecoilValue } from "recoil";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 export default function MovieCardList() {
   const search = useRecoilValue(searchState);
-  const searchMoviesQuery = useQuery({
-    queryKey: ["movie", search],
-    queryFn: () => searchMovies(search),
+  const { data, isFetching, isFetchingNextPage, fetchNextPage, hasNextPage } =
+    useInfiniteQuery({
+      initialPageParam: 1,
+      queryKey: ["movie", search],
+      queryFn: ({ pageParam }) =>
+        searchMovies({ search, page: pageParam, pageSize: 12 }),
+      getNextPageParam: (lastPage) =>
+        lastPage.page ? lastPage.page + 1 : null,
+    });
+
+  const { ref, inView } = useInView({
+    threshold: 0,
   });
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetching && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage]);
   return (
     <div className="grid gap-1 md:grid-cols-4 grid-cols-3 w-full h-full">
-      {searchMoviesQuery.isLoading && <Spinner />}
-      {searchMoviesQuery.data &&
-        searchMoviesQuery.data.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+      {isFetching || (isFetchingNextPage && <Spinner />)}
+      {
+        <>
+          {data?.pages
+            ?.map((page) => page.data)
+            ?.flat()
+            ?.map((movie) => (
+              <MovieCard key={movie.id} movie={movie} />
+            ))}
+          <div ref={ref}></div>
+        </>
+      }
     </div>
   );
 }
